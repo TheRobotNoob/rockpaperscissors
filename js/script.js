953db4fb7eaee95295f7f46957d54c82d4406663
Glitch (rock-the-paper) <none>
2022-01-04 23:26:06 +0000

    🥊🔏 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -6,23 +6,31 @@ $(".play").click(function() {
    console.log(".userChoice");
   $(".userChoice").text(userChoice)
   
-  //Defining RobotChoice based on numbers
-let robotChoice = Math.ceil(Math.random() * 3);
+  //Defining RobotChoice based on number
+function robotChoice() {
+   let robotChoice = Math.ceil(Math.random() * 3);
    $(".computerChoice").text(robotChoice)
+  
   if (robotChoice === 1) {
     computerChoice = "rock";
     $(".computerChoice").text("rock");
   } else if (robotChoice === 2) {
     computerChoice = "scissors"
-    $(".computerChoice").text("sccisors");
+    $(".computerChoice").text("scissors");
   } else if (robotChoice === 3) {
     computerChoice = "paper"
     $(".computerChoice").text("paper");
   }
+  return robotChoice();
+}
+  
+  computerChoice = robotChoice();
+  //$(".computerChoice").text(computerChoice)
+  $(".result").text("rock")
   
   //Choosing winner
   if (userChoice === "paper" && computerChoice === "rock"){
-    $(".result").text("Your win");
+    $(".result").text("Your win"); 
   } else if (userChoice === "paper" && computerChoice === "paper"){
     $(".result").text("Tie");
   } else if (userChoice === "paper" && computerChoice === "scissors") {
@@ -36,6 +44,7 @@ let robotChoice = Math.ceil(Math.random() * 3);
   } else if (userChoice === "rock" && computerChoice === "scissors") {
     $(".result").text("You Win");
   }
+  
     if (userChoice === "scissors" && computerChoice === "scissors") {
     $(".result").text("Tie");
   } else if (userChoice === "scissors" && computerChoice === "rock") {
@@ -44,6 +53,7 @@ let robotChoice = Math.ceil(Math.random() * 3);
     $(".result").text("You Win");
   }
 });
+
 
 
 
